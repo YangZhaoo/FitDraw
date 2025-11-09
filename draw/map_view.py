3623676ff760366937e9d4c87ddeb7ca4b669b31
yz <775901380@qq.com>
2025-11-09 17:39:04 +0800

    优化小地图

--- draw/map_view.py
+++ draw/map_view.py
@@ -26,7 +26,8 @@ class GlobalMap(ViewBase):
         self._image_h, self._image_w = None, None
         self._map = np.zeros((self._map_size, self._map_size, 3),
                              dtype=np.uint8)
-        self._delta_position = None
+        self._map_inner_delta_position = np.array([self._map_edge_size, self._map_edge_size])
+        self._map_out_delta_position = None
         self._min_log_lat = None
         self._max_log_lat = None
 
@@ -37,28 +38,15 @@ class GlobalMap(ViewBase):
         draw_pos = np.array((normailize_log_lat * np.array(
             [[1, -1]]) + np.array([[0, 1]])) * self._map_box_inner_size,
                             dtype=np.int32)
-        # 坐标变化
-        draw_pos += self._delta_position
+        # 距离外框做坐标变化
+        draw_pos += self._map_inner_delta_position
         return draw_pos
 
     def _prepare_data(self, image, records: List[Record]):
         self._image_h, self._image_w = image.shape[:2]
-        self._map = np.zeros_like(image, dtype=np.uint8)
+        self._map = np.zeros((self._map_size, self._map_size, 3), dtype=np.uint8)
         # 小地图的位置 右上角
-        map_box_out_position = np.array([self._image_w - self._map_size, 0])
-
-        self._delta_position = map_box_out_position + np.array([self._map_edge_size, self._map_edge_size])
-        # 边框，debug时使用
-
-        # cv.rectangle(self._map, self._delta_position,
-        #              self._delta_position + np.array(
-        #                  [self._map_box_inner_size, self._map_box_inner_size]),
-        #              (0, 0, 255), thickness=1)
-        # cv.rectangle(self._map, map_box_out_position,
-        #              map_box_out_position + np.array(
-        #                  [self._map_size, self._map_size]),
-        #              (0, 255, 0), thickness=1)
-
+        self._map_out_delta_position = np.array([self._image_w - self._map_size - 100, 100])
         positions = np.asarray(
             [record.location.getNpArray() for record in records if
              record.location is not None])
@@ -69,15 +57,29 @@ class GlobalMap(ViewBase):
         draw_pos = self._convert_position(positions)
         cv.polylines(self._map, [draw_pos], isClosed=False,
                      color=self._map_track_color, thickness=self._map_track_thickness)
+        self._map_mask = np.any(self._map > 0, axis=2)
 
     def _draw(self, record: Record, image, **kargs):
         if not self._prepared:
             self._prepare_data(image, kargs['records'])
             self._prepared = True
-        image = np.where(self._map > 0, self._map, image)
+        image_roi = image[self._map_out_delta_position[1]:self._map_out_delta_position[1]+self._map_size,
+                    self._map_out_delta_position[0]:self._map_out_delta_position[0] + self._map_size,
+                    :]
+        image_roi[self._map_mask] = self._map[self._map_mask]
+
+        # cv.rectangle(image, self._map_out_delta_position + self._map_inner_delta_position,
+        #              self._map_out_delta_position + self._map_inner_delta_position + np.array(
+        #                  [self._map_box_inner_size, self._map_box_inner_size]),
+        #              (0, 0, 255), thickness=1)
+        # cv.rectangle(image, self._map_out_delta_position,
+        #              self._map_out_delta_position + np.array(
+        #                  [self._map_size, self._map_size]),
+        #              (0, 255, 0), thickness=1)
+
         if record.location is None:
             return image
-        current_point = self._convert_position(record.location.getNpArray())[0]
+        current_point = self._convert_position(record.location.getNpArray())[0] + self._map_out_delta_position
         cv.circle(image, current_point, self._current_sign_radius, self._current_sign_color, lineType=cv.LINE_AA,
                   thickness=-1)
         return image
