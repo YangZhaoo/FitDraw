cd6c92f9d60543da22b4f846dac9ae07e642739d
yz <775901380@qq.com>
2025-11-16 15:15:36 +0800

    fix

--- appv2.py
+++ appv2.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
 
     resource_path = '/Users/yangzhao/Desktop/20251001/'
     input_video_dir = os.path.join(resource_path, 'video/')
+    output_video_dir = os.path.join(resource_path, 'video_speed/')
     fit_file = os.path.join(resource_path, f'20251001.fit')
     work_steps = [
         # FrameBlend(skip=True),
@@ -16,16 +17,21 @@ if __name__ == '__main__':
         DebugInfoDraw(False),
         None
     ]
-    video_files = [os.path.join(input_video_dir, file) for file in os.listdir(input_video_dir)[1:2]]
+    all_process_video = set([file for file in os.listdir(input_video_dir) if file.endswith(".MP4")])
+    complete_process_video = set([file.replace('_speed', '') for file in os.listdir(output_video_dir) if file.endswith(".MP4")])
+    need_process_video = all_process_video - complete_process_video
+
+    video_files = [file for file in need_process_video if file.find('20251001123150') > 0]
     count = len(video_files)
     for i, video_file in enumerate(video_files):
         print(f"\n\n{i + 1}/{count}: [{video_file}]\n")
         args_param = {
             'work_steps': work_steps,
-            'input_video_path': video_file,
+            'input_video_path': os.path.join(input_video_dir, video_file),
+            'output_video_path': output_video_dir,
             'record_file_path': fit_file,
-            'preview': False,
-            'draw_box': True
+            'preview': True,
+            'draw_box': False
         }
         process = FitDrawProcess(**args_param)
         process.do_process()
